1edabe16b4de874acc21acacb4ae76297de3be14
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-05-10 22:39:01 +0900

    ViewControllerを修正

--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // UITableView、numberOfRowsInSectionの追加(表示するcellし数を決める)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    // UITableView、cellForRowAtの追加(表示するcellの中身を決める)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let todoCell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        todoCell.textLabel!.text = todoList[indexPath.row]
+        return todoCell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 追加画面で入力した内容を取得する
+        if UserDefaults.standard.object(forKey: "todoList") != nil {
+            todoList = UserDefaults.standard.object(forKey: "todoList") as! [String]
+        }
     }
 
 
